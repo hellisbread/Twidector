e7ef0dd3adac99315e9fb9ad70ad4b1ff1d8c352
rosiecheeks14 <87557506+rosiecheeks14@users.noreply.github.com>
2022-06-22 00:04:41 +0800

    PUSH

---
+++ new_html/js/sideBar.js
@@ -0,0 +1,4 @@
+let sidebarToggle = document.querySelector(".sidebarToggle");
+sidebarToggle.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("active");
+});
