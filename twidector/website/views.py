311957d740a49f5600d9a06e340ae0b4a545bb5d
Nephlheimr <107543268+Nephlheimr@users.noreply.github.com>
2022-07-13 22:10:59 +0800

    Questionable additions

--- twidector/website/views.py
+++ twidector/website/views.py
@@ -2,9 +2,21 @@
 from wsgiref import validate
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.sites.shortcuts import get_current_site
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from .forms import SignupForm
+
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.utils import six
 
 from website.functions import *
 
@@ -77,6 +89,7 @@ def register(request):
 
     if 'loggedin' not in request.session:
         if request.method == 'POST':
+            form = SignupForm(request.POST)
             username = request.POST['username']
             email = request.POST['email']
             password = request.POST['password']
@@ -87,19 +100,55 @@ def register(request):
                 messages.error(request, 'Error. Password does not match.')
                 return redirect('register')
 
-            result = register_user(username, password, usertype, email)
+            if form.is_valid():
 
-            if result:
-                messages.success(request, 'Successfully Created Account')
-                return redirect('login')
-            else:
-                messages.error(request, 'This username may already exist.')
-                return redirect('register')
-            
+                result = register_user(username, password, usertype, email)
+
+                if result:
+                    user = form.save(commit=False)
+                    user.is_active = False
+                    user.save()
+                    current_site = get_current_site(request)
+                    mail_subject = 'Activate your blog account.'
+                    message = render_to_string('acc_active_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token':account_activation_token.make_token(user),
+                })
+                    to_email = form.cleaned_data.get('email')
+                    email = EmailMessage(
+                            mail_subject, message, to=[to_email]
+                )
+                    email.send()
+                    messages.success(request, 'Successfully Created Account')
+                    return redirect('login')
+                else:
+                    messages.error(request, 'This username may already exist.')
+                    return redirect('register')
+
         else:
+            form = SignupForm()
             return render(request, 'register.html', {})
+            
     else:
         return redirect('index')
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        activate_user(request.user.username)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 def forgotPassword(request):
     return render(request, 'forgot-password.html', {})
