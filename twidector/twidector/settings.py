80a15ffa2ee5fd25c6cd270ec3806ed1a6140f6e
Nephlheimr <107543268+Nephlheimr@users.noreply.github.com>
2022-08-01 18:26:47 +0800

    Replace instances of config

--- twidector/twidector/settings.py
+++ twidector/twidector/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
