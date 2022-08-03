b52485ada998aaaa2d76f8833a66773f9102ccd5
Brandon | Laptop <xinomnom@gmail.com>
2022-08-03 13:36:52 +0800

    secret key error fix

--- twidector/manage.py
+++ twidector/manage.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import dotenv
 import sys
 
 
 def main():
     """Run administrative tasks."""
+
+    # dotenv settings
+    dotenv.load_dotenv(
+        os.path.join(os.path.dirname(__file__), '.env')
+    )
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twidector.settings')
+
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
