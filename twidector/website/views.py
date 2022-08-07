5570742d26b22ff49ff8ca5153917e1f5316aaf7
Nephlheimr <107543268+Nephlheimr@users.noreply.github.com>
2022-08-08 01:34:39 +0800

    Update views.py

--- twidector/website/views.py
+++ twidector/website/views.py
@@ -367,8 +367,8 @@ def sync_twitter_callback(request):
             if info is not None:
                 sync_account = SyncTwitterAccount.objects.filter(twitter_id=info[0]['id']).first()
                 if sync_account is None:
-                    current_user = get_user_model()
-                    current_user = current_user.objects.filter(username=current_user.get_username()).first()
+                    user = get_user_model()
+                    current_user = user.objects.filter(username=user.get_username()).first()
                     sync_pair = SyncTwitterAccount(current_user.id, twitter_id=info[0]['id'])
                     sync_pair.save()
                     return redirect('settings')
